457b0f5f9cd49fd433ecb66e2f59c220176d8ec7
gui-hoon <hul1031@naver.com>
2022-12-15 09:33:06 +0900

    aws (x), Dyna (o)

--- src/main/java/com/example/demo/Aws2MariaDbApplication.java
+++ src/main/java/com/example/demo/Aws2MariaDbApplication.java
@@ -30,53 +30,53 @@ public class Aws2MariaDbApplication {
         // 첫 실행 시 data init
         // Dynatrace variable
         
-        GetEntities getentities = new GetEntities();
-		ArrayList<DynaKeyDto> keyList = new ArrayList<>();
-		List<String> entityList = new ArrayList<>();
-		HashMap<String, String> ec2EntityIdMap = new HashMap<String, String>();
-		List<String> metricKeyList = new ArrayList<>();		
-		String environment = null;
-		String token = null;
-		
-		String endTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:00:00'Z'"));
-		String startTime = LocalDateTime.now().minusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:00:00'Z'"));
-		String resolution = "1h";
-		String entityType = "type(\"HOST\")";
-		metricKeyList.add("builtin:host.(cpu.usage,disk.bytesRead,disk.bytesWritten,net.nic.bytesRx,net.nic.bytesTx,mem.usage):(min,max,avg)");
-		metricKeyList.add("builtin:host.disk.free");
-		ConnectDB td = new ConnectDB();
-		
-		try {
-			td.openCon();
+//        GetEntities getentities = new GetEntities();
+//		ArrayList<DynaKeyDto> keyList = new ArrayList<>();
+//		List<String> entityList = new ArrayList<>();
+//		HashMap<String, String> ec2EntityIdMap = new HashMap<String, String>();
+//		List<String> metricKeyList = new ArrayList<>();		
+//		String environment = null;
+//		String token = null;
+//		
+//		String endTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:00:00'Z'"));
+//		String startTime = LocalDateTime.now().minusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:00:00'Z'"));
+//		String resolution = "1h";
+//		String entityType = "type(\"HOST\")";
+//		metricKeyList.add("builtin:host.(cpu.usage,disk.bytesRead,disk.bytesWritten,net.nic.bytesRx,net.nic.bytesTx,mem.usage):(min,max,avg)");
+//		metricKeyList.add("builtin:host.disk.free");
+//		ConnectDB td = new ConnectDB();
+//		
+//		try {
+//			td.openCon();
 //			td.insertEc2Price();
 //			td.insertInstanceDetail();
-			
-			// Dynatrace API
-			keyList = td.selectDynatraceConfig();
-			for(DynaKeyDto key : keyList) {
-				environment = key.getEnvironment();
-				token = key.getToken();
-				
-				entityList = getentities.getEntities(environment, entityType, startTime, endTime, token);
-				
-				for (String entityId : entityList) {
-					ec2EntityIdMap.putAll(getentities.GetEntitiesProperty(environment, entityId, startTime, endTime, token));
-				}
-				
-				for (int i = 0; i < metricKeyList.size(); i++) {
-					for (String entityId : ec2EntityIdMap.keySet()) {
-						td.dataProcessing(environment, metricKeyList.get(i), resolution,
-								Instant.parse(startTime), Instant.parse(endTime), entityId, token, ec2EntityIdMap.get(entityId));
-					}
-				}
-				td.callUpdateAwsconfigIdP();
-			}
-			
-			td.insertAwsResource(Instant.parse(startTime), Instant.parse(endTime));
-			td.closeCon();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//			
+//			// Dynatrace API
+//			keyList = td.selectDynatraceConfig();
+//			for(DynaKeyDto key : keyList) {
+//				environment = key.getEnvironment();
+//				token = key.getToken();
+//				
+//				entityList = getentities.getEntities(environment, entityType, startTime, endTime, token);
+//				
+//				for (String entityId : entityList) {
+//					ec2EntityIdMap.putAll(getentities.GetEntitiesProperty(environment, entityId, startTime, endTime, token));
+//				}
+//				
+//				for (int i = 0; i < metricKeyList.size(); i++) {
+//					for (String entityId : ec2EntityIdMap.keySet()) {
+//						td.dataProcessing(environment, metricKeyList.get(i), resolution,
+//								Instant.parse(startTime), Instant.parse(endTime), entityId, token, ec2EntityIdMap.get(entityId));
+//					}
+//				}
+//				td.callUpdateAwsconfigIdP();
+//			}
+//			
+//			td.insertAwsResource(Instant.parse(startTime), Instant.parse(endTime));
+//			td.closeCon();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
 		// 매일 자정 scheduler 동작
         try {
